b75b5fccf7ee029549af8ceaf4767698773d0fcd
yesseruser <yesseruseryt@gmail.com>
2023-10-06 15:34:14 +0200

    Added full (for now) GameObject implementation.

--- Pong/GameObject.cs
+++ Pong/GameObject.cs
@@ -5,7 +5,24 @@ namespace Pong
 {
     internal class GameObject
     {
+        private readonly SpriteBatch spriteBatch;
+
         public Texture2D Texture { get; }
-        public Vector2 Position { get; }
+        public Vector2 Position { get; set; }
+        public float Scale { get; }
+        public Vector2 Velocity {  get; set; } = Vector2.Zero;
+
+        public GameObject(Texture2D texture, Vector2 position, float scale, SpriteBatch spriteBatch)
+        {
+            Texture = texture;
+            Position = position;
+            Scale = scale;
+            this.spriteBatch = spriteBatch;
+        }
+
+        public void Draw()
+        {
+            spriteBatch.Draw(Texture, Position, null, Color.White, 0, new(Texture.Width / 2, Texture.Height / 2), Scale, SpriteEffects.None, 0);
+        }
     }
 }
