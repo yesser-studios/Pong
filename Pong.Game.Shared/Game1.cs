3968fb016a111f1b08c4d4f86acf8c6072783baf
yesseruser <yesseruseryt@gmail.com>
2023-10-25 20:28:31 +0200

    Added a defaultBallVelocity readonly field.

--- Pong.Game.Shared/Game1.cs
+++ Pong.Game.Shared/Game1.cs
@@ -6,7 +6,8 @@ namespace Pong.Game
 {
     public class Game1 : Microsoft.Xna.Framework.Game
     {
-        const float BALL_SPEED = 3f;
+        private const float BALL_SPEED = 3f;
+        private readonly Vector2 defaultBallVelocity = new Vector2(1f, 0.7f);
 
         private Point gameResolution = new Point(960, 720);
 
@@ -56,7 +57,7 @@ namespace Pong.Game
                 new Vector2(gameResolution.X / 2, gameResolution.Y / 2),
                 1f,
                 _spriteBatch,
-                new Vector2(1f, 1f) * BALL_SPEED);
+                defaultBallVelocity * BALL_SPEED);
 
             leftPad = new GameObject(
                 Content.Load<Texture2D>("Pad"),
