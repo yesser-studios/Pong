15d694ab91d047e27d377865d3c22866ce8f5edf
yesseruser <yesseruseryt@gmail.com>
2024-05-07 19:48:27 +0200

    Add a RichPresence class for managing the SDK

---
+++ DiscordRichPresence/RichPresence.cs
@@ -0,0 +1,13 @@
+﻿namespace DiscordRichPresence;
+
+public class RichPresence
+{
+    public const long CLIENT_ID = 1226792720207450143;
+    
+    public readonly Discord.Discord DiscordObject = new(CLIENT_ID, (ulong)Discord.CreateFlags.Default);
+
+    public void ApplyChanges()
+    {
+        DiscordObject.RunCallbacks();
+    }
+}
