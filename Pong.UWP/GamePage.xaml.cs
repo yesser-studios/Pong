6c514f14abf0e171e28d13fd9dfcf44a28ad7469
yesseruser <yesseruseryt@gmail.com>
2024-05-08 12:27:06 +0200

    Add a ContentDialog explaining the steps to install the new edition

--- Pong.UWP/GamePage.xaml.cs
+++ Pong.UWP/GamePage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Profile;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,7 +19,7 @@ namespace Pong.UWP
 {
     public sealed partial class GamePage : Page
     {
-        readonly Game.Game1 _game;
+        Game.Game1 _game;
 
         public GamePage()
         {
@@ -25,6 +27,16 @@ namespace Pong.UWP
 
             // Disable Xbox overscan (https://learn.microsoft.com/en-us/windows/uwp/xbox-apps/turn-off-overscan)
             Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().SetDesiredBoundsMode(Windows.UI.ViewManagement.ApplicationViewBoundsMode.UseCoreWindow);
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Check if user is on Windows, then prompt install of Desktop edition
+            if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Desktop")
+            {
+                DesktopEditionPrompt prompt = new DesktopEditionPrompt();
+                await prompt.ShowAsync();
+            }
 
             // Create the game.
             var launchArguments = string.Empty;
