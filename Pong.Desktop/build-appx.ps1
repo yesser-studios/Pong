2e2369528356fb9c6f1326f2bf767be759766993
yesseruser <yesseruseryt@gmail.com>
2026-01-27 19:11:04 +0100

    Revert "Add winapp appx"
    This reverts commit b8ffdb7f8a9764c6ad96429fabf1d8323537bf96.

--- Pong.Desktop/build-appx.ps1
+++
@@ -1,6 +0,0 @@
-dotnet build -c Release -r win-x64
-winapp pack .\bin\Release\net8.0\win-x64 --manifest .\appxmanifest.xml --cert .\devcert.pfx --output .\bin\msix\x64.msix
-dotnet build -c Release -r win-x86
-winapp pack .\bin\Release\net8.0\win-x86 --manifest .\appxmanifest.xml --cert .\devcert.pfx --output .\bin\msix\x86.msix
-dotnet build -c Release -r win-arm64
-winapp pack .\bin\Release\net8.0\win-arm64 --manifest .\appxmanifest.xml --cert .\devcert.pfx --output .\bin\msix\arm64.msix
